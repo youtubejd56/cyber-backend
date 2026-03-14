d1e0d635c023bbd3b9ace5c0cb679a35f41968e0
Your Name <you@example.com>
2026-03-14 18:44:46 +0530

    Add build.sh for running migrations on Render

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
+
+echo "Build complete!"
